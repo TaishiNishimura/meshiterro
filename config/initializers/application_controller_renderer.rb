4d245fb0511ac1028cf908c8e19e68997cce0193
TaishiNishimura <taishi19880324@gmail.com>
2021-07-14 15:03:17 +0000

    [finish]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'aee7cd6756f8545dbb6a174ccd5d40352559dcbd03f6a3446dd7ad5fbbc0c67b80e2117713cd13a665058667e12bf36e3f142d81e903ca2e1f0ee65070e0af76'
